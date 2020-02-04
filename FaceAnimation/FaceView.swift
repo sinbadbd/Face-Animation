f958497ff9b62bd8bea444bf3629814681dd11f6
Imran <imufun2@gmail.com>
2020-02-04 22:17:02 +0600

    first commit

---
+++ FaceAnimation/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  FaceAnimation
+//
+//  Created by Sinbad on 4/2/20.
+//  Copyright © 2020 Sinbad. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
