13140f0f7ccf5835cda06339c4b2df1bf5e7fd42
Imran <imufun2@gmail.com>
2020-02-04 22:13:09 +0600

    Initial Commit

---
+++ FaceAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceAnimation
+//
+//  Created by Sinbad on 4/2/20.
+//  Copyright © 2020 Sinbad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
